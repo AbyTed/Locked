63dab912b0bc1a3eba8cec533851e13b93a06cad
AbyTed <119811665+AbyTed@users.noreply.github.com>
2024-01-03 21:20:56 -0500

    Credential input working

--- main.py
+++ main.py
@@ -1,75 +1,96 @@
-import tkinter as tk
 import json
-from defined  import *
-from page import *
+import tkinter as tk
+from utils import *
+from pages import *
 
-root=tk.Tk()
+root = tk.Tk()
 
 # setting the windows size
-root.geometry("600x400")
+root.geometry("800x400")
 
-root.title('Locked')
+root.title("Locked")
 
 
-root.iconbitmap("img/favicon.ico")
+root.iconbitmap("Img/favicon.ico")
 
 
 pop_up_bool = False
 # declaring string variable
 # for storing name and password
-name_var=tk.StringVar()
-passw_var=tk.StringVar()
-FirstUser = Credential('','')
-# new label storage
+name_var = tk.StringVar()
+passw_var = tk.StringVar()
+FirstUser = Credential("", "")
 
 
-# defining a function that will
-# get the name and password and 
-# print them on the screen
-def submit():
-    FirstUser.Username = name_var.get()
-    FirstUser.Password = passw_var.get()
+# storing all
+def main():
+    def submit():
+        FirstUser.Username = name_var.get()
+        FirstUser.Password = passw_var.get()
 
-    passw_var.set("")
-    name_var.set("")
-    if FirstUser.Username == 'A' and FirstUser.Password == 'T':
-        main_page(root, name_var, passw_var,tk)
+        with open("store.json", "r") as open_file:
+            # Reading from json file
+            json_object = json.load(open_file)
+        passw_var.set("")
+        name_var.set("")
+        if (
+            FirstUser.Username == json_object["user"]
+            and FirstUser.Password == json_object["password"]
+        ):
+            main_page(root, name_var, passw_var, tk)
 
-       
-	
-# creating a label for 
-# name using widget Label
-name_label = tk.Label(root, text = 'Username', font=('calibre',10, 'bold'))
-# creating  a popup warning
-# label
-pop_up = tk.Label(root, text = 'Please Enter Valid Credential', font=('calibre',10, 'bold'))
+    # signing up by using existing entries
+    def sign_up():
+        FirstUser.Username = name_var.get()
+        FirstUser.Password = passw_var.get()
 
-# creating a entry for input
-# name using widget Entry
-name_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
+        json_credentials = {"user": FirstUser.Username, "password": FirstUser.Password}
+        with open("store.json", "w") as outfile:
+            json.dump(json_credentials, outfile, indent=2)
 
-# creating a label for password
-passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'bold'))
+    # creating a label for
+    # name using widget Label
+    name_label = tk.Label(root, text="Username", font=("calibre", 10, "bold"))
+    # creating  a popup warning
+    # label
+    pop_up = tk.Label(
+        root,
+        text="Please Enter Valid Credentials \n if you like to sign up please put in credentials \n 1. type your user and password you want \n 2. click sign up \n 3. you then can now input your user and password you have just created",
+        font=("calibre", 10, "bold"),
+    )
 
-# creating a entry for password
-passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+    # creating a entry for input name
+    name_entry = tk.Entry(root, textvariable=name_var, font=("calibre", 10, "normal"))
 
-# creating a button using the widget 
-# Button that will call the submit function 
-sub_btn=tk.Button(root,text = 'Submit', command = submit)
+    # creating a label for password
+    passw_label = tk.Label(root, text="Password", font=("calibre", 10, "bold"))
 
-# sign up button
+    # creating a entry for password
+    passw_entry = tk.Entry(
+        root, textvariable=passw_var, font=("calibre", 10, "normal"), show="*"
+    )
+
+    # creating a button using the widget -> submit()
+    sub_btn = tk.Button(root, text="Submit", command=submit)
+
+    # sign up button
+
+    sign_up = tk.Button(root, text="Sign Up", command=sign_up)
+
+    # placing the label and entry in
+    # the required position using grid
+    # method
+    name_label.grid(row=0, column=0)
+    name_entry.grid(row=0, column=1)
+    passw_label.grid(row=1, column=0)
+    passw_entry.grid(row=1, column=1)
+    sub_btn.grid(row=2, column=1)
+    sign_up.grid(row=2, column=0)
+    pop_up.grid(row=0, column=8)
+    # performing an infinite loop
+    # for the window to display
+    root.mainloop()
 
 
-# placing the label and entry in
-# the required position using grid
-# method
-name_label.grid(row=0,column=0)
-name_entry.grid(row=0,column=1)
-passw_label.grid(row=1,column=0)
-passw_entry.grid(row=1,column=1)
-sub_btn.grid(row=2,column=1)
-# performing an infinite loop 
-# for the window to display
-root.mainloop()
-
+if __name__ == "__main__":
+    main()
--- pages.py
+++ pages.py
@@ -1,23 +1,25 @@
-from defined import *
+from utils import *
 
 
-
-def main_page(root, name_var, passw_var,tk):
+def main_page(root, name_var, passw_var, tk):
     website_var = tk.StringVar()
+
     def add():
         # data -> show on screen and saved temp
-        UserNewCredentials = StoredCredential(passw_var.get(), name_var.get(),website_var.get())
+        UserNewCredentials = StoredCredential(
+            passw_var.get(), name_var.get(), website_var.get()
+        )
         # encryption
-        
+
         # data -> database
         passw_var.set("")
         name_var.set("")
         website_var.set("")
         pass
+
     # creating a label for
     # name using widget Label
     name_label = tk.Label(root, text="add User", font=("calibre", 10, "bold"))
-    
 
     # creating a entry for input
     # name using widget Entry
@@ -31,8 +33,10 @@ def main_page(root, name_var, passw_var,tk):
         root, textvariable=passw_var, font=("calibre", 10, "normal"), show="*"
     )
     website_label = tk.Label(root, text="add website", font=("calibre", 10, "bold"))
-    
-    website_entry = tk.Entry(root, textvariable=website_var, font=("calibre", 10, "normal"))
+
+    website_entry = tk.Entry(
+        root, textvariable=website_var, font=("calibre", 10, "normal")
+    )
 
     # creating a button using the widget
     # Button that will call the submit function
@@ -45,8 +49,6 @@ def main_page(root, name_var, passw_var,tk):
     name_entry.grid(row=0, column=1)
     passw_label.grid(row=1, column=0)
     passw_entry.grid(row=1, column=1)
-    website_label.grid(row=2,column=0)
-    website_entry.grid(row=2,column=1)
+    website_label.grid(row=2, column=0)
+    website_entry.grid(row=2, column=1)
     sub_btn.grid(row=3, column=1)
-
-    
